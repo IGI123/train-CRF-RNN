d6c3e2b70dd64a7dc9d563a33763ba3931a1b045
Rémi Bédard-Couture <remi.bc@outlook.com>
2017-04-06 17:30:20 +0000

    Train with 20 classes

--- filter_images.py
+++ filter_images.py
@@ -12,7 +12,7 @@ from utils import get_id_classes, convert_from_color_segmentation
 def main():
   ## 
   ext = '.png'
-  class_names = ['bird', 'bottle', 'chair']
+  class_names = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
   ## 
 
   path, txt_file = process_arguments(sys.argv)
